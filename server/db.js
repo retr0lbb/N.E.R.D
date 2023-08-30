ee910667765a312edd0ecdce9b8c9d39221fa516
retr0 <retr0lbb@gmail.com>
2023-08-30 14:12:06 -0300

    teste55

--- server/db.js
+++ server/db.js
@@ -11,7 +11,7 @@ let connection;
 
 async function connect(){
     if(!connection){
-        mongoose.connect(`mongodb+srv://retr0lbb:<password>@tcc.ffk0wj3.mongodb.net/?retryWrites=true&w=majority`)
+        mongoose.connect(`mongodb://127.0.0.1:27017/usuario`)
         console.log(connection)
     }
     return connection
