9929f57acc320ea4b71fe64636648a5ec6c0ae92
retr0 <retr0lbb@gmail.com>
2023-09-16 01:31:04 -0300

    added recursive uploads

--- server/config/multer.js
+++ server/config/multer.js
@@ -1,9 +1,16 @@
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs')
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb){
-        cb(null, "uploads/")
+        const uploads = "../uploads";
+
+
+        if(!fs.existsSync(uploads)){
+            fs.mkdirSync(uploads, {recursive: true});
+        }
+        cb(null, uploads)
     },
     filename: function(req, file, cb){
         cb(null, Date.now() + path.extname(file.originalname));
