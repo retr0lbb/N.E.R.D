47ef6504a5993ae564688abcc459454dc960ee09
retr0lbb <retr0lbb@gmail.com>
2023-09-20 18:38:18 +0000

    failure 1

--- server/db.js
+++ server/db.js
@@ -11,7 +11,7 @@ let connection;
 
 async function connect(){
     if(!connection){
-        connection =mongoose.connect(`mongodb://127.0.0.1:27017/Nerd`)
+        //connection =mongoose.connect(`mongodb://127.0.0.1:27017/Nerd`)
         console.log(connection)
     }
     return connection
