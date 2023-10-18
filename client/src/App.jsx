459a3eb95c621a2ac3d3406018465254baa21e66
retr0lbb <retr0lbb@gmail.com>
2023-10-18 00:07:04 +0000

    ola marilene

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Login from './pages/Login'
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import Home from './pages/Home'
+import SingUp from './pages/Singup'
 import './global.css'
 import { ThemeProvider } from '@mui/material'
 import { authTheme } from './themes/authTheme'
@@ -14,6 +15,7 @@ export default function App() {
         <Routes>
           <Route element={<Login />} path="/" exact />
           <Route element={<Home />} path="home" />
+          <Route element={<SingUp />} path='cadastro' />
           <Route path="*" element={<Navigate to='/' />} />
         </Routes>
       </ThemeProvider>
---
+++ client/src/pages/Singup.jsx
@@ -0,0 +1,11 @@
+import react from "react";
+import {Box} from "@mui/material"
+
+export default function SingUp(){
+
+    return(
+        <Box>
+            Hello
+        </Box>
+    )
+}
