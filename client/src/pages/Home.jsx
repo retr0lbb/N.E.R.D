b8c617495c64c0bbfa29dd44f5106d2352d94e7a
saleschan <lipesalessilva2017@gmail.com>
2023-10-26 16:27:42 +0000

    mudei

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -48,7 +48,8 @@ const SearchIconWrapper = styled("div")(({ theme }) => ({
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
   color: "inherit",
   "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
+    padding: theme.spacing(1, 1, 1, 0) 
+    ,
     // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
     transition: theme.transitions.create("width"),
