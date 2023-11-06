432786a846c9079a1d44cbf064cda034d9e8430e
retr0lbb <retr0lbb@gmail.com>
2023-11-06 19:36:36 +0000

    added buy system

--- server/controllers/paymentController.js
+++ server/controllers/paymentController.js
@@ -1,14 +1,13 @@
-
 const Payment = require("../models/payment.model")
 const gameController = require("./GameController")
-
+const Stripe = require("stripe")("SECRET_KEY")
 
 exports.createPayment = async(req, res) =>{
     const {user, products} = req.body;
     try {
 
         if(!products || !user){
-            return res.status(404).send("mamai")
+            return res.status(404).send("Erro ao encontrar produtps selecionados")
         }
 
         const gamePromises = products.map( element =>{
@@ -17,7 +16,20 @@ exports.createPayment = async(req, res) =>{
         const gameResults = await Promise.all(gamePromises)
         const gameBundle = gameResults.filter((e)=> e !== null)
 
-        res.status(200).json({products: gameBundle})
+       
+        var totalPrice = 0;
+        gameBundle.forEach((val) =>{
+            console.log(val.price)
+            totalPrice += val.price
+        })
+        const payment = new Payment({
+            User: user,
+            Product: gameBundle,
+            details: "jogos novos e atualizados",
+            totalValue: totalPrice
+        })
+        res.status(200).json({products: payment})
+
     } catch (error) {
         if(error){
             res.status(500).send("error")
