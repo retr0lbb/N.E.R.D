61a0ed274eaa90995e3c52d1aaf197d02330cea2
saleschan <lipesalessilva2017@gmail.com>
2023-11-27 18:58:25 -0300

    overview fixed

--- client/src/app/pages/Overview.jsx
+++ client/src/app/pages/Overview.jsx
@@ -1,37 +1,59 @@
 import React from 'react'
-import { LayoutBg, Mask } from '../shared/components'
-import { Box, Button } from '@mui/material'
+import CardCover from '@mui/joy/CardCover';
+import { Box, Button, Card } from '@mui/material'
+import tcc from '../assets/img/tcc.jpg'
 
-import NerdLogo from "../shared/assets/img/NerdLogo.png"
+import NerdLogo from "../assets/img/NerdLogo.png"
 
 export default function Overview() {
   return (
-    <LayoutBg>
-      <Mask>
-        <Box sx={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          flexDirection: 'column',
-          width: '40vw',
-          height: '60vh',
-        }}> 
-          <img src={NerdLogo} width={300} className="logo" />
-          <Button
-            sx={{
-              color: 'primary.light',
-              height: 50,
-              width: 275,
-              borderRadius: 2
-            }}
-            variant='contained'
-            href='/home'
-            
-          >
-            Acessar o NERD
-          </Button>
-        </Box>
-      </Mask>
-    </LayoutBg>
+    <Card sx={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: '100vw',
+      height: '100vh'
+    }}>
+      <CardCover>
+        <img
+          src={tcc}
+          style={{
+            zIndex: -1,
+            opacity: 'light',
+            position: 'absolute',
+            left: 0,
+            top: 0,
+            width: '100%',
+            height: '100%',
+            filter: 'blur(5px)'
+          }}
+        />
+      </CardCover>
+      <CardCover sx={{
+        background: 'linear-gradient(0deg, rgba(0, 0, 0, 0.45) 0%, rgba(0, 0, 0, 0.45) 100%), linear-gradient(117deg, rgba(118, 87, 183, 0.90) 0%, rgba(63, 5, 158, 0.90) 50%, rgba(46, 0, 255, 0.90) 100%)',
+        opacity: '0.6'
+      }} />
+      <Box sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        width: '40vw',
+        height: '60vh',
+      }}>
+        <img src={NerdLogo} width={350} className="logo" style={{zIndex: 1}} />
+        <Button sx={{
+          background: `linear-gradient(117deg, rgba(118, 87, 183, 0.79) 0%, rgba(63, 5, 158, 0.79) 50%, rgba(46, 0, 255, 0.79) 100%)`,
+          height: 75,
+          width: 300,
+          borderRadius: 2
+        }}
+          variant='contained'
+          href='/home'
+        >
+          Acessar o NERD
+        </Button>
+      </Box>
+    </Card>
   )
 }
