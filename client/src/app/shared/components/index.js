c39e5c872e5def21ecad4643b26bd512604b9198
retr0lbb <retr0lbb@gmail.com>
2023-12-05 17:22:35 +0000

    ola

--- client/src/app/shared/components/index.js
+++ client/src/app/shared/components/index.js
@@ -1,9 +1,13 @@
 import Layout from "./Layout";
 import LayoutBg from "./LayoutBg";
 import Slide from "./Slide";
+import Mask from "./Mask";
+import styled from "styled-components";
 
 export {
     Layout,
     LayoutBg,
     Slide,
+    Mask,
+    styled
 }
