8f87f9f5a14f591144a266aef98bfbadb2c740d2
Henrique Barbosa Sampaio <retr0lbb@gmail.com>
2024-01-15 22:09:38 -0300

    refactor: starting to do navbar component

--- client/src/pages/Overview.jsx
+++ client/src/pages/Overview.jsx
@@ -26,15 +26,25 @@ export default function Overview() {
     {/** Main */}
 
 
-    <div className="absolute inset-0 flex justify-center items-center flex-col">
-      <img src={NerdLogo} alt="nerd logo with is as nerdemoji written nerd below" />
+    <div className="absolute inset-0 flex justify-center items-center flex-col gap-10 ">
+      <img src={NerdLogo}  alt="nerd logo with is as nerdemoji written nerd below" />
       <button onClick={() => navigator("/home")} className="
       pt-7 pb-7 pl-10 pr-10 
       bg-gradient-to-tl 
       from-secondary 
       to-primary rounded-lg 
       flex justify-center items-center 
-      text-xl font-bold text-text_color "> ACESSAR O CATALOGO</button>
+      text-xl font-bold text-text_color hover:scale-110
+      drop-shadow-md 
+      hover:rounded-3xl 
+      hover:bg-gradient-to-tl 
+      hover:from-primary 
+      hover:to-secondary transition-all
+      ">
+      
+        ACESSAR O CATALOGO
+      
+      </button>
     </div>
     </>
   );
