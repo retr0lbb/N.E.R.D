c07d4520523d9acc160a4874f8b0dd8f3c1ed04e
Henrique Barbosa Sampaio <retr0lbb@gmail.com>
2024-02-03 20:10:15 -0300

    feat: add input componente

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -1,7 +1,21 @@
-import React from "react"
+import { icons } from "lucide-react"
+import React, {InputHTMLAttributes} from "react"
+import {twMerge} from "tailwind-merge"
 
-export const Input: React.FC = () => {
+interface InputCompProps extends InputHTMLAttributes<HTMLInputElement>{
+    Icon?: any
+}
+
+export const InputComp: React.FC<InputCompProps> = ({Icon,...rest}) => {
     return(
-        <div />
+        <div className="w-full flex items-center relative">
+            <input 
+                className={twMerge("p-5 pl-12 bg-black/40 shadow-xl w-full rounded-md text-slate-50 text-sm outline-none", rest.className)}
+                {...rest}
+            />
+            <div className="ml-3 absolute left-0 z-10 text-slate-50">
+                <Icon />
+            </div>
+        </div>
     )
 }
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import NerdLogo from "../static/NerdLogo.png"
 import { Button, Input } from "@nextui-org/react"
 import { Mail, Lock } from "lucide-react"
+import { InputComp } from "../components/Input"
 
 export const Login: React.FC = () => {
     return(
@@ -13,8 +14,8 @@ export const Login: React.FC = () => {
                 <img className="w-96 h-96" src={NerdLogo} alt="Logo da empresa nerd que é focada em jogos digitais logo é uma logo roxa com o emoji de nerd cara redonda com oculos grande e dentes grandes" />
 
                 <div className="w-full grid place-items-center">
-                    <form action="" onSubmit={(e) => e.preventDefault()} className="w-3/4 px-32 flex flex-col gap-6 text-xl">
-                        <Input startContent={<Mail />} label="Email" type="email" isRequired variant="flat" color="secondary"/>
+                    <form action="" onSubmit={(e) => e.preventDefault()} className="w-2/4 flex flex-col gap-10 text-xl">
+                        <InputComp Icon={Mail} type="email" placeholder="Email..." />
                         <Input startContent={<Lock />} label="Password" type="password" isRequired variant="flat" color="secondary"/>
                     </form>
                 </div>
