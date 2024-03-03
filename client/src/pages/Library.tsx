857af6d47f8ae7241f4d5d674da9a0b3744a5ba6
Henrique Barbosa Sampaio <retr0lbb@gmail.com>
2024-03-03 12:53:49 -0300

    minor changes

--- client/src/pages/Library.tsx
+++ client/src/pages/Library.tsx
@@ -27,7 +27,13 @@ export const Library: React.FC = () => {
                         return(
                             <div className="relative overflow-hidden group">
                                 <img className="w-full h-full" src={game.src || ""}/>
-                                <div className="absolute bottom-0 w-full h-20 translate-y-20 group-hover:translate-y-0 transition-all flex items-center justify-between px-5 bg-zinc-950/80">
+                                <div className="absolute bottom-0 
+                                w-full h-20 translate-y-20 
+                                group-hover:translate-y-0 
+                                transition-transform duration-75 
+                                ease-in-out flex items-center
+                                 justify-between px-5 bg-zinc-950/80">
+                                    
                                     <div className="text-2xl text-zinc-200"> {/*Header Components */}
                                         <h1>{game.name}</h1>
                                         <h3 className="text-xs text-zinc-500">Played Time: 1999h</h3>
